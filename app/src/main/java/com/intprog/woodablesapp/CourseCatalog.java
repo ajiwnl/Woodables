fa9ecab61654f8df7fbafadc521b1c7e75675dce
Alvin John Araneta <alvinjohnaranetz@gmail.com>
2024-05-07 23:10:30 +0800

    Push for backup

--- app/src/main/java/com/intprog/woodablesapp/CourseCatalog.java
+++ app/src/main/java/com/intprog/woodablesapp/CourseCatalog.java
@@ -1,6 +1,9 @@
 package com.intprog.woodablesapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +13,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class CourseCatalog extends AppCompatActivity {
 
+    Button toSkillAssess;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_catalog);
+
+        toSkillAssess = findViewById(R.id.skillasses);
+
+        toSkillAssess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toAssessment= new Intent(CourseCatalog.this, AsessmentActivity.class);
+                startActivity(toAssessment);
+            }
+        });
     }
 }
