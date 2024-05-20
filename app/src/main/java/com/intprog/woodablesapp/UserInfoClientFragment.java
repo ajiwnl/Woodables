5b767fa997e6161ce50ea2a1311d7a357babfb52
Alvin John Araneta <alvinjohnaranetz@gmail.com>
2024-05-20 17:56:37 +0800

    Added User Colletion in Firebase -aj

--- app/src/main/java/com/intprog/woodablesapp/UserInfoClientFragment.java
+++ app/src/main/java/com/intprog/woodablesapp/UserInfoClientFragment.java
@@ -10,17 +10,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class UserInfoClientFragment extends Fragment {
 
     Button toReg;
-    EditText lNameText,fNameText,mNameText,cNameText;
+    EditText lNameText, fNameText, mNameText, cNameText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View viewRoot = inflater.inflate(R.layout.fragment_user_info_client, container, false);
 
         lNameText = viewRoot.findViewById(R.id.lName);
@@ -33,18 +31,16 @@ public class UserInfoClientFragment extends Fragment {
         toReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                UserInfoActivity activity = (UserInfoActivity) getActivity();
                 Intent toRegister = new Intent(viewRoot.getContext(), RegisterActivity.class);
 
                 toRegister.putExtra("LName", lNameText.getText().toString());
                 toRegister.putExtra("FName", fNameText.getText().toString());
                 toRegister.putExtra("MName", mNameText.getText().toString());
                 toRegister.putExtra("CName", cNameText.getText().toString());
+                toRegister.putExtra("Role", activity.getSelectedRole());
 
                 startActivity(toRegister);
-
-                Toast.makeText(viewRoot.getContext(), toRegister.getExtras().toString() , Toast.LENGTH_LONG).show();
             }
         });
 
