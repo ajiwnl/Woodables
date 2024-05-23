efddba92e6d12edea8cedbb01c10792d200bd889
Alvin John Araneta <alvinjohnaranetz@gmail.com>
2024-05-23 19:10:59 +0800

    waiting for editprofile to fix the ui

--- app/src/main/java/com/intprog/woodablesapp/EditProfileActivity.java
+++ app/src/main/java/com/intprog/woodablesapp/EditProfileActivity.java
@@ -30,9 +30,9 @@ public class EditProfileActivity extends AppCompatActivity {
         userId = mAuth.getCurrentUser().getUid();
 
         Button saveButton = findViewById(R.id.saveEdit);
-        EditText firstNameEditText = findViewById(R.id.editName1);
-//        EditText middleNameEditText = findViewById(R.id.editMiddleName);
-//        EditText lastNameEditText = findViewById(R.id.editLastName);
+        EditText firstNameEditText = findViewById(R.id.editFirstName);
+        EditText middleNameEditText = findViewById(R.id.editMiddleName);
+        EditText lastNameEditText = findViewById(R.id.editLastName);
         EditText desc2EditText = findViewById(R.id.profileDesc2);
         EditText desc3EditText = findViewById(R.id.profileDesc3);
         EditText desc4EditText = findViewById(R.id.profileDesc4);
@@ -48,8 +48,8 @@ public class EditProfileActivity extends AppCompatActivity {
 
         saveButton.setOnClickListener(v -> {
             String firstName = firstNameEditText.getText().toString();
-//            String middleName = middleNameEditText.getText().toString();
-//            String lastName = lastNameEditText.getText().toString();
+            String middleName = middleNameEditText.getText().toString();
+            String lastName = lastNameEditText.getText().toString();
             String desc2 = desc2EditText.getText().toString();
             String desc3 = desc3EditText.getText().toString();
             String desc4 = desc4EditText.getText().toString();
@@ -57,9 +57,9 @@ public class EditProfileActivity extends AppCompatActivity {
             String desc6 = desc6EditText.getText().toString();
             String desc7 = desc7EditText.getText().toString();
 
-            if (!firstName.isEmpty()) {
+            if (!firstName.isEmpty() || !middleName.isEmpty() || !lastName.isEmpty()) {
                 db.collection("users").document(userId)
-                        .update("First Name", firstName)
+                        .update("First Name", firstName, "Middle Name", middleName, "Last Name", lastName)
                         .addOnSuccessListener(aVoid -> {
                             Toast.makeText(EditProfileActivity.this, "Profile updated", Toast.LENGTH_SHORT).show();
                         })
@@ -68,7 +68,7 @@ public class EditProfileActivity extends AppCompatActivity {
                         });
             }
 
-            String fullName = firstName;
+            String fullName = firstName + " " + middleName + " " + lastName;
 
             Intent intent = new Intent();
             intent.putExtra("FULL_NAME", fullName);
