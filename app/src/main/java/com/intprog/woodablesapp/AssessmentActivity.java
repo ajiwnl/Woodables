6f5ad2a9a968910eebb3acd95eb318cc8dfea4ea
UnusualRuWei <moradafranzruie@gmail.com>
2024-05-26 03:33:43 +0800

    Fragmentation, Joblisting Creation, and UI Fixes

--- app/src/main/java/com/intprog/woodablesapp/AssessmentActivity.java
+++ app/src/main/java/com/intprog/woodablesapp/AssessmentActivity.java
@@ -8,7 +8,11 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -34,7 +38,14 @@ public class AssessmentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_assessment);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         // Initialize Firestore and FirebaseAuth
         db = FirebaseFirestore.getInstance();
--- app/src/main/java/com/intprog/woodablesapp/ForgotPasswordActivity.java
+++ app/src/main/java/com/intprog/woodablesapp/ForgotPasswordActivity.java
@@ -1,5 +1,6 @@
 package com.intprog.woodablesapp;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -21,6 +22,7 @@ public class ForgotPasswordActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_forgot_password);
 
         mAuth = FirebaseAuth.getInstance();
